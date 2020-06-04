fba5a5fec5a98846535d5b5cbe4df5630182a534
Vishwas-Acharya <vishwasacharya2016@gmail.com>
2020-06-04 16:29:50 +0530

    04/06/2020

---
+++ coding_solutions/program21.py
@@ -0,0 +1,13 @@
+#Python program to find H.C.F of two numbers
+def compute_hcf(x,y):
+    if x>y:
+        smaller=y
+    else:
+        smaller=x
+    for i in range(1,smaller+1):
+            if((x%i==0)and(y%i==0)):
+                hcf=i
+            return hcf
+num1=54
+num2=24
+print("TheH.C.F.is",compute_hcf(num1,num2))
