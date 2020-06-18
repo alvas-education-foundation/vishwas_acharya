984d4d534136b803630188fdcd65bb3950e79d7e
Vishwas-Acharya <vishwasacharya2016@gmail.com>
2020-06-18 15:42:04 +0530

    18/06/2020

---
+++ coding_solutions/program35.py
@@ -0,0 +1,15 @@
+# Python program to find nth magic numebr 
+def nthMagicNo(n): 
+  
+    pow = 1
+    answer = 0
+    while (n): 
+  
+        pow = pow*5
+        if (n & 1): 
+            answer += pow
+        n >>= 1
+      
+    return answer 
+n = 5
+print("nth magic number is", nthMagicNo(n)) 
